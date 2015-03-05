9627a2e78d1b2c956029e710675588163f260645
Adam <ovadia.adam@gmail.com>
2015-03-05 14:03:22 -0500

    Update and rename liquid.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -2,21 +2,19 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
-# Get the long list of APNs
-$(call inherit-product, vendor/liquid/config/telephony.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/geeb/full_geeb.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := geeb
-PRODUCT_NAME := liquid_geeb
+PRODUCT_NAME := validus_geeb
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG Optimus G
 PRODUCT_MANUFACTURER := LGE
